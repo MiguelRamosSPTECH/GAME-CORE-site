1676b8f22c542b3c664a5e9029ff34a60c4f2d63
manuelamiyuki <manuela.matsumoto@sptech.school>
2025-10-02 16:03:52 -0300

    início do cadastro de servidor e arrumar parte do cadastro e login

--- public/js/cadastrarServidor.js
+++ public/js/cadastrarServidor.js
@@ -1,3 +1,83 @@
-function enviarCadastroServidor(){
+function enviarCadastroServidor() {
     
+    var hostnameVar = ipt_hostname.value;
+    var ipVar = ipt_ip.value;
+    var localizacaoVar = ipt_localizacao.value;
+    var componenteVar = ipt_componente.value;
+    var metricaVar = metrica.value;
+
+    let metricasSelecionadas = []
+    let componentesSelecionados = []
+
+
+    document.querySelectorAll("input[type=checkbox]:checked").forEach(c => {
+        metricasSelecionadas.push(c.value)
+    })
+    document.querySelectorAll("input[type=checkbox]:checked").forEach(c => {
+        componentesSelecionados.push(c.value)
+    })
+
+    if (
+        
+        hostnameVar == "" ||
+        ipVar == "" ||
+        localizacaoVar == "" ||
+        componenteVar == "" ||
+        metricaVar == ""
+        
+    ) {
+        cardErro.style.display = "block";
+        mensagem_erro.innerHTML =
+            "(Mensagem de erro para todos os campos em branco)";
+
+        finalizarAguardar();
+        return false;
+    } else {
+        setInterval(sumirMensagem, 5000);
+    }
+
+    fetch("/cadastrarServidor/enviarCadastroServidor", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            // crie um atributo que recebe o valor recuperado aqui
+            // Agora vá para o arquivo routes/usuario.js
+
+            hostname: hostnameVar,
+            ip: ipVar,
+            localizacao: localizacaoVar,
+            componente: componenteVar,
+            metrica: metricaVar
+        }),
+    })
+        .then(function (resposta) {
+            console.log("resposta: ", resposta);
+
+            if (resposta.ok) {
+                cardErro.style.display = "block";
+
+                mensagem_erro.innerHTML =
+                    "Cadastro realizado com sucesso!";
+
+                setTimeout(() => {
+                    // window.location = "login.html";
+                }, "2000");
+
+                // limparFormulario();
+                finalizarAguardar();
+            } else {
+                throw "Houve um erro ao tentar realizar o cadastro!";
+            }
+        })
+        .catch(function (resposta) {
+            console.log(`#ERRO: ${resposta}`);
+            finalizarAguardar();
+        });
+
+    return false;
+
+
+
 }
---
+++ src/controllers/cadastroServidorController.js
@@ -0,0 +1,42 @@
+var cadastroServidorModel = require("../models/cadastroServidorModel");
+
+function cadastrarServidor(req,res){
+    var hostname = req.body.hostnameServer
+    var ip = req.body.ipServer
+    var localizacao = req.body.localizacaoServer
+    var metrica = req.body.metricaServer
+    var componente = req.body.componenteServer
+
+    var idEmpresa = req.session.usuario.fk_empresa_servidor;
+
+    if (hostname == undefined) {
+        res.status(400).send("O nome hostname está indefinido!");
+    } else if (ip == undefined) {
+        res.status(400).send("Seu ip está indefinido!");
+    } else if (localizacao == undefined) {
+        res.status(400).send("A localização está indefinida!");
+    } else if (metrica == undefined) {
+        res.status(400).send("Selecione ao menos 1 métrica!");
+    } else if (componente == undefined) {
+        res.status(400).send("Selecione ao menos 1 componente!"); 
+    } else if (idEmpresa == undefined) {
+        res.status(400).send("Sessão inválida! ID da empresa não identificado!");
+    }
+    else{
+        cadastroServidorModel.enviarCadastroServidor(hostname,ip,localizacao,metrica,componente)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+}
