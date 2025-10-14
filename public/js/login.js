2f7b7c9b72607b6e710be7930ce4156817d68cf2
HenryCaetano <henry.silva@sptech.school>
2025-10-13 23:17:20 -0300

    Alterações: (quase) finalização da criação de cargo

--- public/js/login.js
+++ public/js/login.js
@@ -39,6 +39,7 @@ function entrar() {
                     sessionStorage.EMAIL_USUARIO = json.email;
                     sessionStorage.NOME_USUARIO = json.nome;
                     sessionStorage.ID_USUARIO = json.id;
+                    //sessionStorage.ID_EMPRESA = json.id;
 
                     setTimeout(function () {
                         window.location = "index.html";
