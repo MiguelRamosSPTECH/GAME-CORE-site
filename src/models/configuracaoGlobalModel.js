cdf1028491f9d7c4e457cf7ee97b16281aa1e2f4
pedrorico79 <pedro.rico@sptech.school>
2025-10-18 16:52:46 -0300

    inicio criação config global

---
+++ src/models/configuracaoGlobalModel.js
@@ -0,0 +1,15 @@
+const { atualizar } = require("../controllers/configuracaoGlobalController");
+var database = require("../database/config");
+
+function listar(){
+    var instrucao = ``;
+}
+
+function atualizar(componente,novaMetrica){
+    var instrucao = ``;
+}
+
+module.exports = {
+    listar,
+    atualizar
+};
