a1e83a3310466c1a3db949459d076c626016cd0d
pedrorico79 <pedro.rico@sptech.school>
2025-10-23 13:17:28 -0300

    continuando backend

--- src/models/configModel.js
+++ src/models/configModel.js
@@ -1,18 +1,26 @@
 var database = require("../database/config");
 
-function criarLayout(nomeLayout,ListaChecked,fk_empresa_cargo){
+async function criarLayout(nomeLayout,ListaChecked,fk_empresa_cargo){
     console.log("model config")
-
-    // pegar vcalores de alerta
-
-// inserir nome do layout e fk emopresa
 
 // for + select (componente e metrica) pra pegar cada componente e metrica
 
+// pegar alertas
+
 // insert na tabela config do server
 
-    let 
+    // insert tabela layout
+    let insertLayout = `
+        INSERT INTO layout (nome,fk_empresa_layout)
+        VALUES ('${nomeLayout}','${fk_empresa_cargo}');
+    `
+    let returnInsertLayout = await database.executar(insertLayout);
+    let idLayout = returnInsertLayout.insertId;
 
+    // for pra pegar cada metrica
+    for (var i = 0; i < ListaChecked.length; i++){
+        console.log(ListaChecked[i])
+    }
 
 
 
