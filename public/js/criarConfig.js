3bd63aee62f4a3928f97529114a204ad5e7def8a
pedrorico79 <pedro.rico@sptech.school>
2025-10-23 00:58:40 -0300

    commit da madrugada, às 00:58, sabendo que mexeram nos meus arquivos enquanto eu desenvolvia, prestes a ter que resolver o maior merge da minha vida

---
+++ public/js/criarConfig.js
@@ -0,0 +1,59 @@
+
+
+function criarLayout() {
+
+    let listaChecked = [];
+    let fk_empresaVar = sessionStorage.ID_EMPRESA;
+
+    let componentesChecked = document.getElementsByClassName('check-componente');
+
+    for (let i = 0; i < componentesChecked.length; i++){
+
+        if(componentesChecked[i].style.backgroundColor == "rgb(116, 206, 58)"){
+
+            for(let j = 0; j < ((componentesChecked[i].parentNode.parentNode.parentNode).lastElementChild.children).length; j++){
+
+                if((componentesChecked[i].parentNode.parentNode.parentNode).lastElementChild.children[j].children[0].checked){
+
+                    let x = (componentesChecked[i].parentNode.innerText).trim().replaceAll(" ","_")
+
+                    listaChecked.push({[x]:`${(componentesChecked[i].parentNode.parentNode.parentNode).lastElementChild.children[j].innerText}`})
+
+                }
+            }
+
+        }
+
+    }
+    console.log(listaChecked)
+
+    let nomeLayoutVar = document.getElementById("ipt_nome").value;
+
+    if (nomeLayoutVar == null || nomeLayoutVar.trim() == ""){
+        alert("preencha o nome")
+    } else if(listaChecked.length != 4){
+        alert("Deve escolher apenas 4 componentes")
+    } else{
+        fetch("/config/criarLayout", {
+            method: "POST",
+            headers:{
+                "Content-Type":"application/json",
+            },
+            body:JSON.stringify({
+                nomeLayoutServer: nomeLayoutVar,
+                ListaCheckedServer: listaChecked,
+                fk_empresaServer: fk_empresaVar
+
+            })
+        })
+            .then(function (resposta) {
+                console.log("Resposta: ", resposta);
+
+                // if(resposta.ok){}
+            })
+            .catch(function (resposta) {
+                console.log(`#ERRO: ${resposta}`);
+            });
+            return false;
+    }
+}
---
+++ src/models/configModel.js
@@ -0,0 +1,17 @@
+var database = require("../database/config");
+
+function criarLayout(nomeLayout,ListaChecked,fk_empresa_cargo){
+    console.log("model config")
+
+
+// inserir nome do layout e fk emopresa
+
+// for + select (componente e metrica) pra pegar cada componente e metrica
+
+// insert na tabela config do server
+
+}
+
+module.exports = {
+    criarLayout
+};
