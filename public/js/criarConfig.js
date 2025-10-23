c4bf59b3754914a633043a6779cd4b59cc8e7f1f
MiguelRamosSPTECH <miguel.ramos@sptech.school>
2025-10-23 03:28:43 -0300

    lógica de configurar componentes completa (estatica) e modal de visualizar chamado feito

--- public/js/criarConfig.js
+++ public/js/criarConfig.js
@@ -1,1 +1,9 @@
-
+var radiosInputs = document.querySelectorAll('input[type="radio"]')
+for(let i=0;i<radiosInputs.length;i++) {
+  radiosInputs[i].addEventListener('click', ()=> {
+    var areaInputs = radiosInputs[i].parentNode.parentNode.parentNode.lastElementChild
+      if(radiosInputs[i].checked) {
+        areaInputs.setAttribute('style','display:flex!important')
+      }
+  })
+}
