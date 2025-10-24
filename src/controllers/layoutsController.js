345cfa4d715deecff33e8439607f698b0f4d28f2
pedrorico79 <pedro.rico@sptech.school>
2025-10-24 18:21:01 -0300

    pagina layouts de configs funcionando, bugs corrigidos

--- src/controllers/layoutsController.js
+++ src/controllers/layoutsController.js
@@ -12,19 +12,48 @@ function buscar(req, res) {
 
 }
 
-function buscarCompleto(req,res) {
+function buscarCompleto(req, res) {
     const idEmpresa = req.params.idEmpresa;
 
     layoutsModel.buscarCompleto(idEmpresa)
-    .then(result => res.status(201).json(result))
-    .catch(erro => {
-        console.log(erro)
-        res.status(500).json(erro.sqlMessage);
-    })
-    ;
+        .then(result => res.status(201).json(result))
+        .catch(erro => {
+            console.log(erro)
+            res.status(500).json(erro.sqlMessage);
+        })
+        ;
+}
+
+function usarLayout(req, res) {
+    var idLayout = req.body.idLayoutServer;
+    var idEmpresa = req.body.idEmpresaServer;
+
+    if (idLayout == undefined) {
+        res.status(400).send("O idlayout está indefinido!");
+    } else if (idEmpresa == undefined) {
+        res.status(400).send("Seu idempresa está indefinido!");
+    } else {
+        layoutsModel.usarLayout(idLayout, idEmpresa)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            )
+            .catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
 }
 
 module.exports = {
     buscar,
-    buscarCompleto
+    buscarCompleto,
+    usarLayout
 };
