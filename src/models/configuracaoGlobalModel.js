e367ccda0c548c452dcffc362aed771ff5a5c516
pedrorico79 <pedro.rico@sptech.school>
2025-10-24 01:15:57 -0300

    bd/back

--- src/models/configuracaoGlobalModel.js
+++ src/models/configuracaoGlobalModel.js
@@ -2,14 +2,18 @@ const { atualizar } = require("../controllers/configuracaoGlobalController");
 var database = require("../database/config");
 
 function listar(){
-    var instrucao = ``;
+    var instrucao = `
+        SELECT  layout.nome,
+                componente.nome,
+                metrica.unidadeMedida
+    `;
 }
 
-function atualizar(componente,novaMetrica){
-    var instrucao = ``;
-}
+// function atualizar(componente,novaMetrica){
+//     var instrucao = ``;
+// }
 
 module.exports = {
     listar,
-    atualizar
+    // atualizar
 };
