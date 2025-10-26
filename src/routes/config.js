268dfc247c74e93272ecebccf0e9b6cdfa3aa20d
pedrorico79 <pedro.rico@sptech.school>
2025-10-26 02:30:03 -0300

    backend: edição de layout funcionando!, listar layouts funcionando

--- src/routes/config.js
+++ src/routes/config.js
@@ -4,7 +4,16 @@ var router = express.Router();
 var configController = require("../controllers/configController.js");
 
 router.post("/criarLayout", function (req, res) {
-    configController.criarLayout(req,res);
+    configController.criarLayout(req, res);
+});
+
+router.get("/listarLayout/:idLayout/:idEmpresa", (req, res) => {
+    console.log("controler listarlayout")
+    configController.listarLayout(req, res);
+})
+
+router.put("/editarLayout/:idLayout", function (req, res) {
+    configController.editarLayout(req, res);
 });
 
 module.exports = router;
