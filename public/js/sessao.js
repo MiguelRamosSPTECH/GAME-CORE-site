f8ad68a68be8c4f5ed2383492a2517a93d918875
MiguelRamosSPTECH <miguel.ramos@sptech.school>
2025-10-26 10:46:35 -0300

    a

--- public/js/sessao.js
+++ public/js/sessao.js
@@ -3,6 +3,7 @@ function validarSessao() {
 
     var nomeCargo = sessionStorage.NOME_CARGO
     var quebraURL = window.location.href.split(`/`)
+    console.log(quebraURL)
     if(quebraURL[3] == "dashboard") {
         if(quebraURL[4] == "dash_adm" && nomeCargo != "Administrador Master") {
             limparSessao()
