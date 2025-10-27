2512a3961f5e254dc9db09ceee3f0b5a57c0adbb
HenryCaetano <henry.silva@sptech.school>
2025-10-27 14:06:38 -0300

    Alterações: Administrador Master pode acessar as telas da dash

--- public/js/sessao.js
+++ public/js/sessao.js
@@ -5,13 +5,13 @@ function validarSessao() {
     var quebraURL = window.location.href.split(`/`)
     console.log(quebraURL)
     if(quebraURL[3] == "dashboard") {
-        if(quebraURL[4] == "dash_adm" && nomeCargo != "Administrador Master") {
+        if(quebraURL[4] == ("dash_sre" || "dash_adm") && nomeCargo != "Administrador Master") {
             limparSessao()
         } else if(quebraURL[5] == "create_servidor.html" && nomeCargo != "Administrador Master"){
             limparSessao()
         } else if(quebraURL[4] == "dash_sre" && nomeCargo != "Engenheiro SRE"){
             limparSessao()
-        } else if(nomeCargo != "GAMEOPS" && nomeCargo != "Administrador Master" && quebraURL[4] != ("dash_sre" || "dash_adm")) { 
+        } else if(quebraURL[4] == ("dash_sre" || "dash_adm") && nomeCargo != "GAMEOPS") { 
             limparSessao()
         }
     }
