13cefbb14a7f4ef050239a03c817eba98d0c1548
GustavoZiliotto <gustavo.bello@sptech.school>
2025-10-31 00:24:47 -0300

    logica comentada terminar amanhã

--- src/controllers/aceitarEmpresasController.js
+++ src/controllers/aceitarEmpresasController.js
@@ -51,39 +51,40 @@ function editar(req, res) {
 
 }
 
-// function buscar_Cargos(req, res) {
-//     console.log("indo para o AceitarEmpresasController - buscar_Cargos");
+function buscar_Cargos(req, res) {
+    console.log("indo para o AceitarEmpresasController - buscar_Cargos");
 
-//     var filtro = req.body.filtroServer;
+    var filtro = req.body.filtroServer;
 
-//         aceitarEmpresaModel.buscar_Cargos(filtro)
-//             .then(
-//                 function (resultadoBuscarCards) {
-//                     console.log(`\nResultados encontrados: ${resultadoBuscarCards.length}`);
-//                     console.log(`Resultados: ${JSON.stringify(resultadoBuscarCards)}`); // transforma JSON em String
+        aceitarEmpresaModel.buscar_Cargos(filtro)
+            .then(
+                function (resultadoBuscarCards) {
+                    console.log(`\nResultados encontrados: ${resultadoBuscarCards.length}`);
+                    console.log(`Resultados: ${JSON.stringify(resultadoBuscarCards)}`); // transforma JSON em String
 
-//                     if (resultadoBuscarCards.length >= 1) {
-//                         console.log(resultadoBuscarCards);
+                    if (resultadoBuscarCards.length >= 1) {
+                        console.log(resultadoBuscarCards);
 
-//          // mudado para retornar todos os valores do select e não só o primeiro como antes
-//          res.json(resultadoBuscarCards);
+         // mudado para retornar todos os valores do select e não só o primeiro como antes
+         res.json(resultadoBuscarCards);
 
 
-//                     } else if (resultadoBuscarCards.length == 0) {
-//                         res.status(403).send("filtro inválido(s)");
-//                     }
-//                 }
-//             ).catch(
-//                 function (erro) {
-//                     console.log(erro);
-//                     console.log("\nHouve um erro ao buscar os cards! Erro: ", erro.sqlMessage);
-//                     res.status(500).json(erro.sqlMessage);
-//                 }
-//             );
-// }
+                    } else if (resultadoBuscarCards.length == 0) {
+                        res.status(403).send("filtro inválido(s)");
+                    }
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log("\nHouve um erro ao buscar os cards! Erro: ", erro.sqlMessage);
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+}
 
 
 module.exports = {
     buscar_cards,
-    editar
+    editar,
+    buscar_Cargos
 }
