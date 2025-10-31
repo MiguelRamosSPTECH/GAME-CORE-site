da47bfb2bb58eafa7cd5e0813f86cd2c7359b974
GustavoZiliotto <gustavo.bello@sptech.school>
2025-10-31 17:57:04 -0300

    tela aprovar empresa funcionando

--- src/models/aceitarEmpresasModel.js
+++ src/models/aceitarEmpresasModel.js
@@ -37,8 +37,44 @@ function buscar_Cargos(filtro) {
     return database.executar(instrucaoSql);
 }
 
+function atualizar_perfil(novoStatus, idEmpresa) {
+    console.log("ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function editar(): ", novoStatus, idEmpresa);
+    var instrucaoSql = `
+        UPDATE Funcionario f
+        INNER JOIN Cargo c ON f.fk_cargo_func = c.id
+        INNER JOIN Empresa e ON c.fk_empresa_cargo = e.id
+        SET f.perfilAtivo = ${novoStatus}
+        WHERE e.id = ${idEmpresa};
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+
+function criar_perfil(novoStatus, idEmpresa, nome) {
+    console.log("ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function editar(): ", novoStatus, idEmpresa);
+    var emailFuncionario = `${nome}@email.com`;
+    var senhaFuncionario = `${nome}${Math.floor(Math.random() * 10000)}`;
+
+    
+    var instrucaoSql = `
+       START TRANSACTION;
+
+        INSERT INTO Cargo (nome, fk_empresa_cargo, ativo)
+        VALUES ('Administrador Master', ${idEmpresa}, 1);
+
+        INSERT INTO Funcionario (nome, email, senha, perfilAtivo, fk_cargo_func)
+        VALUES ('Administrador Master', '${emailFuncionario}', '${senhaFuncionario}', 1, LAST_INSERT_ID());
+
+        COMMIT;
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
 module.exports = {
     buscar_cards,
     editar,
-    buscar_Cargos
+    buscar_Cargos,
+    atualizar_perfil,
+    criar_perfil
 };
