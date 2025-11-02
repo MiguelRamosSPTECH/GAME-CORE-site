ac1ea7845be62812a1dda2195809a6616e03a480
MiguelRamosSPTECH <miguel.ramos@sptech.school>
2025-11-02 01:29:47 -0300

    Criação do script shell com container docker e banco mysql utilizando docker-compose

---
+++ config.sh/config_amb_gamecore_v2.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+DOCKER_COMPOSE_VERSION="v2.24.5"
+
+#------------BAIXANDO O DOCKER-----------------#
+echo "BAIXANDO DOCKER NA INSTÂNCIA..."
+sudo apt-get update 
+sudo apt-get install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+echo "EXECUTANDO ARQUIVO DE INSTALAÇÃO DO DOCKER"
+
+
+echo "DOCKER baixado e configurado com sucesso!!!"
+echo sudo docker -v
+#----------------BAIXANDO DOCKER COMPOSE-----------------#
+echo "Instalando o docker compose...."
+
+#garante que seja pro LINUX na arquitetura correta (x64, x86) e salva no diretório padrão do sistema p poder usar o comando em todo o SO
+sudo curl -L "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+#permissao de execucao (precaucao)
+sudo chmod +x /usr/local/bin/docker-compose
+
+echo "Versão do docker compose:"
+/usr/local/bin/docker-compose version
+
+#-------------------------------------------------------------#
+
+#CONFIGS MYSQL
+DIR_CONFIGS="/config-containers"
+COMPOSE_FILE="$DIR_CONFIGS/docker-compose.yml"
+
+#DADOS MYSQL
+MYSQL_ROOT_PASSWORD="gamecore123"
+
+echo "Criando diretório de configuração dos containers"
+sudo mkdir -p "$DIR_CONFIGS"
+
+echo "Criando arquivo docker-compose.yml..."
+
+sudo cat > "$COMPOSE_FILE" << EOF
+version: '3.8'
+
+services:
+  db: 
+    image: miguelramoslimadev/atividade-dockerfile:latest    
+    container_name: mysql-gamecore
+    restart: unless-stopped
+    ports:
+    - "3306:3306"
+    volumes:
+    - mysql-data:/var/lib/mysql
+    environment:
+      MYSQL_ROOT_PASSWORD: "$MYSQL_ROOT_PASSWORD"
+
+volumes:
+  mysql-data:
+EOF
+
+echo "==============================="
+echo "Iniciando container MYSQL isolado com o Docker compose men"
+
+cd "$DIR_CONFIGS"
+#aqui ele pega o docker-compose.yml executa mas libera o terminal tlgd
+sudo /usr/local/bin/docker-compose up -d
+
+echo "VERIFICANDO EXECUÇÃO DO CONTAINER MYSQL"
+sudo /usr/local/bin/docker-compose ps
+
+
+echo "================== ADICIONANDO UBUNTU AO GRUPO DOCKER =================================="
+sudo usermod -aG docker ubuntu
---
+++ config.sh/criacao_ec2_gamecore.sh
@@ -0,0 +1,44 @@
+
+#!/bin/bash
+
+nome_chave_pem=chaveaws
+id_vpc=$(aws ec2 describe-vpcs --query "Vpcs[0].[VpcId]" --output text)
+echo "ID do VPC $id_vpc ..."
+
+echo "================================================"
+echo "PEGANDO SECURITY GROUP"
+aws ec2 create-security-group --group-name launch-wizard-42 --vpc-id "$id_vpc" --description "grupo_de_seguranca_042" --tag-specifications 'ResourceType=security-group,Tags=[{Key=Name,Value=sg-042}]'
+id_sg=$(aws ec2 describe-security-groups --filters Name=group-name,Values=launch-wizard-42 --query "SecurityGroups[0].[GroupId]" --output text)
+echo "ID Security Group $id_sg ..."
+
+echo "================================================"
+echo "PEGANDO SUBNET"
+id_subnet=$(aws ec2 describe-subnets --query "Subnets[0].[SubnetId]" --output text)
+echo "ID Subnet $id_subnet ..."
+
+echo "================================================"
+echo "CRIANDO CHAVE.PEM"
+aws ec2 create-key-pair --key-name $nome_chave_pem --region us-east-1 --query 'KeyMaterial' --output text > $nome_chave_pem.pem
+echo "Chave $nome_chave_pem.pem criada com sucesso."
+
+chmod 400 "$nome_chave_pem.pem"
+
+aws ec2 authorize-security-group-ingress --group-id $id_sg --protocol tcp --port 80 --cidr 0.0.0.0/0
+aws ec2 authorize-security-group-ingress --group-id $id_sg --protocol tcp --port 22 --cidr 0.0.0.0/0
+aws ec2 authorize-security-group-ingress --group-id $id_sg --protocol tcp --port 3306 --cidr 0.0.0.0/0
+
+echo "================================================"
+echo "CRIANDO INSTANCIA"
+aws ec2 run-instances \
+  --image-id ami-0360c520857e3138f \
+  --count 1 \
+  --security-group-ids $id_sg \
+  --instance-type t3.micro \
+  --subnet-id $id_subnet \
+  --key-name $nome_chave_pem \
+  --user-data file://config_amb_gamecore_v2.sh \
+  --block-device-mappings '[{"DeviceName":"/dev/sda1","Ebs":{"VolumeSize":20,"VolumeType":"gp3","DeleteOnTermination":true}}]' \
+  --tag-specifications 'ResourceType=instance,Tags=[{Key=Name, Value=web-server-01}]'
+
+
+
