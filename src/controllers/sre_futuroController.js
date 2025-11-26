5533b3bde9ffc08380ef322df538ab0e6814edb0
GustavoZiliotto <gustavo.bello@sptech.school>
2025-11-26 19:48:30 -0300

    dash ind merge, falta só s3

---
+++ src/controllers/sre_futuroController.js
@@ -0,0 +1,69 @@
+var sre_futuroModel = require("../models/sre_futuroModel");
+    console.log("aasadasdasdas 123123123123");
+
+function entrar(req, res) {
+
+    console.log("aasadasdasdas");
+    
+
+
+    var id = req.body.idServer;
+
+
+    if (id == undefined) {
+        res.status(400).send("Seu id está undefined!");
+    } else {
+        sre_futuroModel.entrar(id)
+            .then(
+                function (resultadoAutenticar) {
+                    console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
+                    console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
+                     res.json(resultadoAutenticar);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+
+}
+
+function buscar(req, res) {
+
+    console.log("aasadasdasdas");
+    
+
+
+    var id = req.body.idServer;
+
+
+    if (id == undefined) {
+        res.status(400).send("Seu id está undefined!");
+    } else {
+        sre_futuroModel.entrar(id)
+            .then(
+                function (resultadoAutenticar) {
+                    console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
+                    console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
+                     res.json(resultadoAutenticar);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+
+}
+
+
+
+module.exports = {
+    entrar,
+    buscar
+}
---
+++ src/routes/sre_futuro.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var router = express.Router();
+
+var sre_futuroController = require("../controllers/sre_futuroController");
+
+router.post("/entrar", function (req, res) {
+    sre_futuroController.entrar(req, res);
+});
+
+router.post("/buscar", function (req, res) {
+    sre_futuroController.entrar(req, res);
+});
+
+module.exports = router;
+
