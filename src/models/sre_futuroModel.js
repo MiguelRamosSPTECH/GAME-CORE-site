9a7b3f08796d47189a584a85bc903bc3fda8024a
GustaZiliotto <gustavo.bello@sptech.school>
2025-11-28 14:31:28 -0300

    conexão bucket e logica enciminhadas

--- src/models/sre_futuroModel.js
+++ src/models/sre_futuroModel.js
@@ -21,6 +21,8 @@ function buscar(id) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", id)
     var instrucaoSql = `
         select
+        id,
+        macadress,
         apelido, 
     localizacao
     from Servidor   
