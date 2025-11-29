06ad985cfd3d7694a13617b80d0b42d7266c5fc6
pedrorico79 <pedro.rico@sptech.school>
2025-11-28 21:23:33 -0300

    add funções s3, tratamento de dados e exibição nas kpis

--- public/js/dash_temperatura.js
+++ public/js/dash_temperatura.js
@@ -41,23 +41,7 @@ function buscarParametrosDashTemp() {
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//grafico 1 ---------------------------------------------
 const ctxTemp = document.getElementById('myChartTemp');
 const labelsTemp = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul'];
 const dataTemp = {
@@ -66,7 +50,7 @@ const dataTemp = {
         label: 'My First Dataset',
         data: [65, 59, 80, 81, 56, 55, 40],
         fill: false,
-        borderColor: 'rgb(75, 192, 192)',
+        borderColor: 'rgba(38, 173, 173, 1)',
         tension: 0.1
     }]
 };
@@ -84,12 +68,16 @@ const configTemp = {
                 display: true,
                 text: 'Temperatura'
             }
-        }
+        },
+        backgroundColor: 'rgba(38, 173, 173, 0.4)'
     },
 };
 
 new Chart(ctxTemp, configTemp);
+//grafico 1 ---------------------------------------------
 
+
+//grafico 2 ---------------------------------------------
 const ctxUso = document.getElementById('myChartUso');
 const labelsUso = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul'];
 const dataUso = {
@@ -98,7 +86,7 @@ const dataUso = {
         label: 'My First Dataset',
         data: [65, 59, 80, 81, 56, 55, 40],
         fill: false,
-        borderColor: 'rgb(75, 192, 192)',
+        borderColor: 'rgba(169, 63, 218, 1)',
         tension: 0.1
     }]
 };
@@ -116,8 +104,213 @@ const configUso = {
                 display: true,
                 text: 'Uso'
             }
-        }
+        },
+        backgroundColor: 'rgba(169, 63, 218, 0.4)'
     },
 };
 
 new Chart(ctxUso, configUso);
+//grafico 2 ---------------------------------------------
+
+// S3 ---------------------------------------------
+async function buscarArquivoPedroProc() {
+    const resposta = await fetch(`/dashTemperatura/dados_pedro/dados_processos.csv`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then(async resposta => {
+            var processos = await resposta.json();
+            if (resposta.ok) {
+                // console.log(resposta);
+                // console.log(processos);
+                // console.log(processos[0].timestamp);
+                // const dateObj = new Date(processos[0].timestamp.replace(" ", "T"));
+                // console.log(dateObj);
+                // console.log(typeof dateObj);
+                // const atual = new Date();
+                // console.log(atual);
+                // console.log(typeof atual);
+                // console.log(top5ProcessosCpu(processos))
+
+                // return processos;
+            }
+        })
+}
+
+async function buscarArquivoPedroMed() {
+    const resposta = await fetch(`/dashTemperatura/dados_pedro/dados_capturados.json`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then(async resposta => {
+            var medicoes = await resposta.json();
+            if (resposta.ok) {
+
+                // console.log(resposta);
+                // console.log(medicoes);
+
+                // atualizarKpis(medicoes);
+
+                // return medicoes;
+            }
+        })
+}
+// S3 ---------------------------------------------
+
+function top5ProcessosCpu(allProcessos) {
+    const dataAtual = new Date();
+    // const data30MinAtras = new Date(dataAtual.getTime() - 30*60*1000); // const data30MinAtras = new Date(dataAtual.getTime() - 30 * 60 * 1000); // DATA PARA FUNCIONAR NORMALMENTE
+    const data30MinAtras = new Date("2025-11-28T10:15:30"); // data fixa para teste
+
+    console.log(data30MinAtras)
+    const processosFiltrados = allProcessos.filter(p => {
+        return new Date(p.timestamp.replace(" ", "T")) >= data30MinAtras;
+    })
+    return processosFiltrados;
+}
+
+function atualizarKpis(allMedicoes) {
+    const dataAtual = new Date();
+
+
+    // const data30MinAtras = new Date(dataAtual.getTime() - 30 * 60 * 1000); // DATA PARA FUNCIONAR NORMALMENTE
+    const data30MinAtras = new Date("2025-11-19T21:03:30"); // data fixa para teste 
+
+
+    const medicoesFiltradas = allMedicoes.filter(m => {
+        return new Date(m.timestamp.replace(" ", "T")) >= data30MinAtras;
+    })
+
+    // console.log(medicoesFiltradas[0].cpu_porcentagem, medicoesFiltradas[medicoesFiltradas.length - 1].cpu_porcentagem);
+    const usoPrimeiraMedicao = medicoesFiltradas[0].cpu_porcentagem;
+    const usoAtualMedicao = medicoesFiltradas[medicoesFiltradas.length - 1].cpu_porcentagem;
+    const variacaoUso = (usoAtualMedicao - usoPrimeiraMedicao).toFixed(1);
+    // console.log(variacaoUso);
+    // return medicoesFiltradas;
+
+    console.log(medicoesFiltradas);
+
+    if (variacaoUso >= 0) {
+        stringVariacaoUso = `+${variacaoUso}`;
+    } else {
+        stringVariacaoUso = `-${variacaoUso}`;
+    }
+
+    document.getElementById('variacaoCpu').innerHTML = stringVariacaoUso;
+
+    document.getElementById('atualCpu').innerHTML = usoAtualMedicao.toFixed(1);
+
+    const valorKpiUso = document.getElementById('atualCpu');
+    const simboloUso = document.getElementById('atualCpuPorcent');
+    valorKpiUso.classList.remove('cor1', 'cor2', 'cor3', 'cor4');
+    simboloUso.classList.remove('cor1', 'cor2', 'cor3', 'cor4');
+
+    const limiteUsoA1 = parseFloat(document.getElementById('uso-a1').textContent);
+    const limiteUsoA2 = parseFloat(document.getElementById('uso-a2').textContent);
+
+    if (usoAtualMedicao <= limiteUsoA1) {
+        valorKpiUso.classList.add('cor1');
+        simboloUso.classList.add('cor1');
+    } else if (usoAtualMedicao <= limiteUsoA2) {
+        valorKpiUso.classList.add('cor3');
+        simboloUso.classList.add('cor3');
+    } else {
+        valorKpiUso.classList.add('cor4');
+        simboloUso.classList.add('cor4');
+    }
+
+    // ---------------------------------------------
+    const tempPrimeiraMedicao = medicoesFiltradas[0].temperatura_cpu;
+    const tempAtualMedicao = medicoesFiltradas[medicoesFiltradas.length - 1].temperatura_cpu;
+    const variacaoTemp = (tempAtualMedicao - tempPrimeiraMedicao).toFixed(1);
+
+    // console.log(medicoesFiltradas)
+    // console.log(medicoesFiltradas[medicoesFiltradas.length -1].temperatura_cpu)
+    // console.log(medicoesFiltradas[medicoesFiltradas.length -1].timestamp)
+    // console.log(medicoesFiltradas[0].temperatura_cpu)
+    // console.log(medicoesFiltradas[0].timestamp)
+    if (variacaoTemp >= 0) {
+        stringVariacaoTemp = `+${variacaoTemp}`;
+    } else {
+        stringVariacaoTemp = `-${variacaoTemp}`;
+    }
+    document.getElementById('variacaoTemp').innerHTML = stringVariacaoTemp;
+
+    document.getElementById('atualTemp').innerHTML = tempAtualMedicao.toFixed(1);
+
+    const valorKpiTemp = document.getElementById('atualTemp');
+    const simboloPorcent = document.getElementById('atualTempPorcent')
+    valorKpiTemp.classList.remove('cor1', 'cor2', 'cor3', 'cor4');
+    simboloPorcent.classList.remove('cor1', 'cor2', 'cor3', 'cor4');
+
+    const limiteTempA1 = parseFloat(document.getElementById('temp-a1').textContent);
+    const limiteTempA2 = parseFloat(document.getElementById('temp-a2').textContent);
+
+    if (tempAtualMedicao <= limiteTempA1) {
+        valorKpiTemp.classList.add('cor1');
+        simboloPorcent.classList.add('cor1');
+    } else if (tempAtualMedicao <= limiteTempA2) {
+        valorKpiTemp.classList.add('cor3');
+        simboloPorcent.classList.add('cor3');
+    } else {
+        valorKpiTemp.classList.add('cor4');
+        simboloPorcent.classList.add('cor4');
+    }
+
+
+    // -----------------------------
+    const tempoAcimaLimite = calcularTempoAcimaLimite(medicoesFiltradas, limiteTempA1);
+    // console.log(tempoAcimaLimite);
+
+    document.getElementById('limite-temp-crit').innerHTML = limiteTempA1;
+    document.getElementById('tempo-acima-limite').innerHTML = tempoAcimaLimite + ' min';
+
+    const tempoAcumulado = document.getElementById('tempo-acima-limite');
+    tempoAcumulado.classList.remove('cor1', 'cor2', 'cor3', 'cor4');
+
+    if (tempoAcimaLimite < 2) {
+        tempoAcumulado.classList.add('cor1');
+    } else if (tempoAcimaLimite <= 5) {
+        tempoAcumulado.classList.add('cor2');
+    } else if (tempoAcimaLimite <= 12) {
+        tempoAcumulado.classList.add('cor3');
+    } else {
+        tempoAcumulado.classList.add('cor4');
+    }
+
+    // ---------------------------------------------
+
+    const freqMinDisponivel = medicoesFiltradas[medicoesFiltradas.length - 1].cpuFrequenciaMin;
+    const freqAtual = medicoesFiltradas[medicoesFiltradas.length - 1].cpuFrequencia;
+    const freqMaxDisponivel = medicoesFiltradas[medicoesFiltradas.length - 1].cpuFrequenciaMax;
+
+    document.getElementById('freq-min').textContent = freqMinDisponivel.toFixed(1);
+    document.getElementById('freq-atual').textContent = freqAtual.toFixed(1);
+    document.getElementById('freq-max').textContent = freqMaxDisponivel.toFixed(1);
+
+}
+
+function calcularTempoAcimaLimite(medicoesFiltradas, limiteTemperatura) {
+    let tempoAcumulado = 0;
+
+    for (let i = 0; i < medicoesFiltradas.length - 1; i++) {
+        const medicaoAtual = medicoesFiltradas[i];
+        const proximaMedicao = medicoesFiltradas[i + 1];
+
+        if (medicaoAtual.temperatura_cpu > limiteTemperatura) {
+
+            const tempoAtual = new Date(medicaoAtual.timestamp.replace(" ", "T"));
+            const tempoProximo = new Date(proximaMedicao.timestamp.replace(" ", "T"));
+            const diferenca = (tempoProximo - tempoAtual) / 1000;
+
+            tempoAcumulado += diferenca;
+        }
+    }
+
+    return Math.round(tempoAcumulado / 60);
+}
+
--- src/routes/dashTemperatura.js
+++ src/routes/dashTemperatura.js
@@ -1,11 +1,19 @@
 var express = require("express");
 var router = express.Router();
+const path = require('path');
 
 var dashTemperaturaController = require("../controllers/dashTemperaturaController");
 
-
 router.get("/buscarParametros/:idEmpresa", (req, res) => {
     dashTemperaturaController.buscarParametros(req, res);
+});
+
+router.get('/dados_pedro/:arquivo', (req, res) => {
+    dashTemperaturaController.lerArquivoPedroProc(req, res);
+});
+
+router.get('/dados_pedro/:arquivo', (req, res) => {
+    dashTemperaturaController.lerArquivoPedroMed(req, res);
 });
 
 module.exports = router;
